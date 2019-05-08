c5a4cf51e64b8f28e3ced7ff15ae6f70dcf61a6c
ivditi <ivditi@ivditis-Air.home>
2019-05-07 23:27:43 -0400

    added queue

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = { users: [] }
+  
+  componentDidMount() {
+   fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +29,6 @@ function App() {
       </header>
     </div>
   );
-}
+}}
 
 export default App;
